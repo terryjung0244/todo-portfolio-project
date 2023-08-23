3ef4173d1e010e6ec519d3065447b735e1978dfb
terryjung0244 <terryjung0244@gmail.com>
2023-08-23 19:53:39 -0400

    Created createTodo Component

--- src/components/createTodo/CreateTodo.tsx
+++ src/components/createTodo/CreateTodo.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Styled from './CreateTodo.Styled';
+import { CreateInputStateType } from './CreateTodo.interface';
+import { useAppDispatch } from 'service/store';
+import { createTodoAction } from 'service/redux/action/todoAction';
+import { getNanoid } from 'service/util/nanoId';
 
 const CreateTodo = () => {
+  const dispatch = useAppDispatch();
+  const [createInput, setCreateInput] = useState<CreateInputStateType>({
+    todo: '',
+  });
+
+  const createInputFunc = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCreateInput({ ...createInput, [e.target.name]: e.target.value });
+  };
+
+  const createInputAddBtn = () => {
+    dispatch(createTodoAction({ ...createInput, id: getNanoid() }));
+  };
+
   return (
     <Styled.CreateTodo>
-      <div>CreateTodo</div>
+      <input
+        name="todo"
+        value={createInput.todo}
+        placeholder="Enter Todo"
+        onChange={createInputFunc}
+      />
+      <button onClick={createInputAddBtn}>ADD</button>
     </Styled.CreateTodo>
   );
 };
--- src/service/redux/action/todoAction.ts
+++ src/service/redux/action/todoAction.ts
@@ -1,1 +1,11 @@
-export {};
+import { TODO_ACTIONS_CONST } from 'service/const/todoActionConst';
+import { TodoType } from 'service/model/todoGeneral';
+
+const { CREATE_TODO } = TODO_ACTIONS_CONST;
+
+export const createTodoAction = (todoInput: TodoType) => {
+  return {
+    type: CREATE_TODO,
+    payload: todoInput,
+  };
+};
